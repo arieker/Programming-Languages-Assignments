379133b43c98b431e75364a76591c1defdeeffa5
arieker <riekeranthony@gmail.com>
2024-04-18 15:12:35 -0400

    End of class

---
+++ C#/Test 4/Test4Problem_1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+abstract class Person
+{
+    public virtual void sayIt()
+    {
+        Console.WriteLine("Generic output");
+    }
+
+    public void doIt()
+    {
+        Console.WriteLine("Doing it...");
+    }
+}
+
+class PsychologyMajor : Person
+{
+    public override void sayIt()
+    {
+        Console.WriteLine("PsychologyMajor output");
+    }
+}
+
+class ComputerScienceMajor : Person
+{
+    public override void sayIt()
+    {
+        Console.WriteLine("ComputerScienceMajor output");
+    }
+}
+
+class Barista : Person
+{
+    public override void sayIt()
+    {
+        Console.WriteLine("Barista output");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        PsychologyMajor a = new PsychologyMajor();
+        ComputerScienceMajor b = new ComputerScienceMajor();
+        Barista c = new Barista();
+        Person f;
+
+        f = (Person)a;
+        f.sayIt();
+        f = (Person)b;
+        f.sayIt();
+        f = (Person)c;
+        f.sayIt();
+    }
+}
---
+++ C#/Test 4/Test4_Problem2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Thread thread1 = new Thread(CountByOnes);
+        Thread thread2 = new Thread(CountByTwos);
+        Thread thread3 = new Thread(CountBySevens);
+
+        thread1.Start();
+        thread2.Start();
+        thread3.Start();
+    }
+
+    static void CountByOnes()
+    {
+        for (int i = 1; i <= 10000; i++)
+        {
+            Console.WriteLine($"1:{i}:");
+        }
+    }
+
+    static void CountByTwos()
+    {
+        for (int i = 2; i <= 20000; i += 2)
+        {
+            Console.WriteLine($"2:{i}:");
+        }
+    }
+
+    static void CountBySevens()
+    {
+        for (int i = 7; i <= 70000; i += 7)
+        {
+            Console.WriteLine($"3:{i}:");
+        }
+    }
+}
